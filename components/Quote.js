bda449cc83ada2bc84e8f37a5272aebb96ed6d00
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2017-06-28 17:55:34 +0300

    added Quote component

---
+++ components/Quote.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { accentColor } from '../lib/colors';
+import { bodyFont, headingFont } from '../lib/fonts';
+import media from '../lib/media';
+
+const QuoteBlock = styled.div`
+  box-sizing: border-box;
+  margin: 0 1.5em 2em 1.5em;
+  padding: 0 1.5rem;
+  border-left: 1px solid ${accentColor};
+
+  ${media.tablet`
+    margin: 0 auto 2em auto;
+  `}
+
+  ${media.desktop`
+    margin: 0 auto 2em auto;
+  `}
+`;
+
+const QuoteText = styled.div`
+  margin: 0 0 1em 0;
+  font-family: ${headingFont};
+  font-size: 1.3rem;
+  font-style: italic;
+`;
+
+const QuoteAuthorName = styled.div`
+  font-family: ${bodyFont};
+  font-size: 0.9rem;
+`;
+
+const QuoteAuthorPosition = styled.div`
+  font-family: ${bodyFont};
+  font-size: 0.8rem;
+  opacity: 0.5;
+`;
+
+const Quote = ({ text, author, position }) =>
+  (<QuoteBlock>
+    <QuoteText>{text}</QuoteText>
+    <QuoteAuthorName>{author}</QuoteAuthorName>
+    <QuoteAuthorPosition>{position}</QuoteAuthorPosition>
+  </QuoteBlock>);
+
+Quote.propTypes = {
+  text: PropTypes.string,
+  author: PropTypes.string,
+  position: PropTypes.string,
+};
+
+Quote.defaultProps = {
+  text: '',
+  author: '',
+  position: '',
+};
+
+export default Quote;
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,12 @@ import Heading from '../components/Heading';
 import Image from '../components/Image';
 import Link from '../components/Link';
 import Paragraph from '../components/Paragraph';
+import Quote from '../components/Quote';
 import Text from '../components/Text';
 import { UList, UListItem, OList, OListItem } from '../components/List';
 
-export default () => (
-  <PostWrapper>
+export default () =>
+  (<PostWrapper>
     <Post>
       <PostHeader
         date="14 may 2017"
@@ -18,38 +19,44 @@ export default () => (
       />
       <PostContent>
         <Paragraph>
-            After a few weeks of playing around with React Native, I just came
-            away
-            with my first real mobile app. It’s fairly simple, but it only took me
-            a
-            few days to build and I had a blast doing it. I created a mobile app
-            for
-            my favorite food blog, Smitten Kitchen. The app allows users to search
-            through a database of over 1,000 recipes, and concisely view the
-            necessary ingredients and directions for each one. Users can also
-            bookmark recipes and easily{' '}
+          After a few weeks of playing around with React Native, I just came
+          away
+          with my first real mobile app. It’s fairly simple, but it only took me
+          a
+          few days to build and I had a blast doing it. I created a mobile app
+          for
+          my favorite food blog, Smitten Kitchen. The app allows users to search
+          through a database of over 1,000 recipes, and concisely view the
+          necessary ingredients and directions for each one. Users can also
+          bookmark recipes and easily{' '}
           <Link href="https://google.com">share them with a friend</Link>.
-          </Paragraph>
+        </Paragraph>
 
         <Paragraph>
-            I’m still waiting for permission from the blog to publish this app,
-            but
-            you can check out all of the code here. Note that the url for my API
-            has
-            been stubbed for the time being out of respect for Smitten Kitchen.
-          </Paragraph>
+          I’m still waiting for permission from the blog to publish this app,
+          but
+          you can check out all of the code here. Note that the url for my API
+          has
+          been stubbed for the time being out of respect for Smitten Kitchen.
+        </Paragraph>
 
         <Heading>React Native isn’t going away any time soon</Heading>
 
         <Paragraph>
-            A common reservation among developers is that they don’t want to
-            invest
-            the time to learn a new technology if there’s a strong chance it will
-            become obsolete in the near future. Even from my relatively minimal
-            experience with React Native, I’ve found it to be an enormously
-            powerful
-            tool. I am confident it will be used in the years to come.
-          </Paragraph>
+          A common reservation among developers is that they don’t want to
+          invest
+          the time to learn a new technology if there’s a strong chance it will
+          become obsolete in the near future. Even from my relatively minimal
+          experience with React Native, I’ve found it to be an enormously
+          powerful
+          tool. I am confident it will be used in the years to come.
+        </Paragraph>
+
+        <Quote
+          text="The app allows users to search through a database of over 1,000 recipes, and concisely view the necessary ingredients and directions for each one."
+          author="Elon Mask"
+          position="CEO Tesla"
+        />
 
         <Image
           src="http://rcysl.com/wp-content/uploads/2017/02/Pics-Of-Nature-1920x1080.jpg"
@@ -57,24 +64,24 @@ export default () => (
         />
 
         <Text>
-            After a few weeks of playing around with React Native, I just came:
-          </Text>
+          After a few weeks of playing around with React Native, I just came:
+        </Text>
 
         <UList>
           <UListItem>
-              After a few weeks of playing asdk qjqwje hajhkf hahsflq ahshfasfas
-            </UListItem>
+            After a few weeks of playing asdk qjqwje hajhkf hahsflq ahshfasfas
+          </UListItem>
           <UListItem>After a few weeks of playing</UListItem>
         </UList>
 
         <Text>
-            After a few weeks of playing around with React Native, I just came:
-          </Text>
+          After a few weeks of playing around with React Native, I just came:
+        </Text>
 
         <OList>
           <OListItem>
-              After a few weeks of playing asdk qjqwje hajhkf hahsflq ahshfasfas
-            </OListItem>
+            After a few weeks of playing asdk qjqwje hajhkf hahsflq ahshfasfas
+          </OListItem>
           <OListItem>After a few weeks of playing</OListItem>
         </OList>
 
@@ -83,5 +90,4 @@ export default () => (
         <Text>text</Text>
       </PostContent>
     </Post>
-  </PostWrapper>
-  );
+  </PostWrapper>);
